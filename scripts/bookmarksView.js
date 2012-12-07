2809fb01c83b802f59cb3a0f1ea79819e9af79ad
Alden Quimby <aldenquimby@gmail.com>
2012-12-07 00:25:08 -0500

    temp

--- scripts/bookmarksView.js
+++ scripts/bookmarksView.js
@@ -13,6 +13,9 @@ var BookmarksView = Backbone.View.extend({
     initialize: function() {
       var self = this;
 
+      dispatcher.on(appEvents.bookMarkPlaceFinish, function (bkmrk) {
+        self.addBookmark(bkmrk);
+      });
       dispatcher.on(appEvents.persistResultsReturned, function (data) {
         self.displayBookmarks(data);
       });
@@ -55,14 +58,14 @@ var BookmarksView = Backbone.View.extend({
     displayBookmarks: function (data) {
       var self = this;
       self.$el.find('.results').html('');
-      //self.displayMessage(data.businesses.length, self.lastSearch.query, self.lastSearch.location);
       _.each(data, function(bkmrk){
         self.$el.find('.results').append(new BookmarkCardView({model:bkmrk.data}).el);
       })
     },
 
-    displayMessage: function() {
-
+    addBookmark: function(bkmrk) {
+      persistApi.set(bkmrk.id, bkmrk);
+      self.$el.find('.results').append(new BookmarkCardView({model:bkmrk}).el);
     }
 
 });
