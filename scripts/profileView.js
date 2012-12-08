ad950628373a49b49a3e22b0c2eedf272e4894a1
Colfax Selby <cole31_31@yahoo.com>
2012-12-08 03:19:37 -0500

    working on profile view

--- scripts/profileView.js
+++ scripts/profileView.js
@@ -16,22 +16,42 @@ var ProfileView = Backbone.View.extend({
     // app, we set a direct reference on the model for convenience.
     initialize: function() {
       var self = this;
+
+//      var yelpApi = new YelpApiDebug(self.onApiError);
+
       var businessObject = self.options.smallModel;
       //for now
       self.model = businessObject;
 
       dispatcher.on(appEvents.showSearchPage, function (){
         self.$el.remove();
-      })
+      });
+
+      console.log("right before api call");
+      console.log(businessObject);
+
+      yelpApi.business(dispatcher.trigger("got " + businessObject[0]), businessObject[0]);//here************* what am i doing wrong?
+
+      console.log("right after api call");
+
       //businessObject is the thing that has the id in it so you can do the yelp business call
-      self.render();
+      dispatcher.listen("got " + businessObject[0], function (data){//here***************** what am i doing wrong?
+        self.render(data)
+      });
+
+//      yelpApi.business(self.render, businessObject[0]);    
+
     },
 
     // Re-render the titles of the todo item.
-    render: function() {
+    render: function (data) {
       var self = this;
-      self.$el.mustache(self.template, self.model, { method:'html' });
+      self.$el.mustache(self.template, data, { method:'html' });
       return self;
+    },
+
+    onApiError: function () {
+      alert("something bad happened with the api");
     },
 
     showStuff: function () {
@@ -42,7 +62,6 @@ var ProfileView = Backbone.View.extend({
     }
 
   });
-
 
 
 
