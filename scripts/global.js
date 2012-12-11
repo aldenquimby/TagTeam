d98b0af2a88c4964f5286411e1ed580b72579439
Alden Quimby <aldenquimby@gmail.com>
2012-12-11 11:08:21 -0500

    order of javascript files was messing things up... bootstrap last ftw!!!

--- scripts/global.js
+++ scripts/global.js
@@ -41,60 +41,62 @@ var persistApi = new StoreDataDebug(function(){
 	dispatcher.trigger(appEvents.apiError, 'We are having trouble loading your bookmarks, please try again later.');
 });
 
-var tagTeamTutorial = new Tutorial([{
-    title:'Search for Places', 
-    target:'#search-query', 
-    content:"Enter a keyword, like 'dive bar' to find great places near you.",
-    placement:'bottom',
-    transistionNextCallback: function() {
-        if ($('.search .result').length == 0) {
-            $('#search-query').val(appDefaults.query);
-            $('#search-submit').click();
+var tagTeamTutorial = new Tutorial([
+    {
+        title:'Search for Places', 
+        target:'#search-query', 
+        content:"Enter a keyword, like 'dive bar' to find great places near you.",
+        placement:'bottom',
+        transistionNextCallback: function() {
+            if ($('.search .result').length == 0) {
+                $('#search-query').val(appDefaults.query);
+                $('#search-submit').click();
+            }
         }
+    },
+    {
+        title:'Browse Search Results', 
+        target:'#search-results-span', 
+        content:'View search results here.',
+        placement:'left'
+    },
+    {
+        title:'Advanced Searching',
+        target:'#search-filter', 
+        content:'After searching, sort and filter the search results here.',
+        placement:'left'
+    },
+    {
+        title:'Business Profile',
+        target:'.search .result-name', 
+        content:'View more detailed info about a business by clicking the name.',
+        placement:'bottom',
+        transistionNextCallback: function() {
+            $($('.search .result-name')[0]).find('a').click();
+        }
+    },
+    {
+        title:'Business Profile',
+        target:'.profile .prof-details', 
+        content:'Read reviews, locate the business on a map and more.',
+        placement:'left'
+    },
+    {
+        title:'Bookmark',
+        target:'.profile .edit-bookmark .book', 
+        content:'Bookmark this place for later by clicking here.',
+        placement:'bottom'
+    },
+    {
+        title:'My Bookmarks',
+        target:'.tab.bookmarkTab', 
+        content:'Browse your existing bookmarks here.',
+        placement:'right'
+    },
+    {
+        title:'Help and Documentation',
+        target:'.tab.helpTab', 
+        content:'Questions? Click here for help and documentation.',
+        placement:'top'
     }
-},
-{
-    title:'Browse Search Results', 
-    target:'#search-results-span', 
-    content:'View search results here.',
-    placement:'left'
-},
-{
-    title:'Advanced Searching',
-    target:'#search-filter', 
-    content:'After searching, sort and filter the search results here.',
-    placement:'left'
-},
-{
-    title:'Business Profile',
-    target:'.search .result-name', 
-    content:'View more detailed info about a business by clicking the name.',
-    placement:'bottom',
-    transistionNextCallback: function() {
-        $($('.search .result-name')[0]).find('a').click();
-    }
-},
-{
-    title:'Business Profile',
-    target:'.profile .prof-details', 
-    content:'Read reviews, locate the business on a map and more.',
-    placement:'left'
-},
-{
-    title:'Bookmark',
-    target:'.profile .edit-bookmark .book', 
-    content:'Bookmark this place for later by clicking here.',
-    placement:'bottom'
-},
-{
-    title:'My Bookmarks',
-    target:'.tab.bookmarkTab', 
-    content:'Browse your existing bookmarks here.',
-    placement:'right'
-},
-{
-    title:'Help and Documentation',
-    target:'.tab.helpTab', 
-    content:'Questions? Click here for help and documentation.',
-    placement:'top'
-}]);
+]);
